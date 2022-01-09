365694e4c6e0b6ffcd2175bedc72f6d3233d3fd2
Hussein <gimble20@yahoo.com>
2022-01-09 10:11:46 -0500

    add prompt for player weapon selection each round

---
+++ io
@@ -0,0 +1,17 @@
+[33mcommit 7e469ee80bf602246b76e931bbcd55479f3d577c[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: Hussein <gimble20@yahoo.com>
+Date:   Sun Jan 9 09:56:26 2022 -0500
+
+    make game workable in console
+
+[33mcommit 84ab9bb80d7f12a6aba0b9b018b889dcceadf3ec[m
+Author: Hussein <gimble20@yahoo.com>
+Date:   Wed Jan 5 23:59:44 2022 -0500
+
+    add files
+
+[33mcommit c47d152e180d54218efbe571f4ea5571c16e3fba[m
+Author: gimble20 <94493175+gimble20@users.noreply.github.com>
+Date:   Wed Jan 5 23:54:42 2022 -0500
+
+    Initial commit
--- js/scipt.js
+++ js/scipt.js
@@ -1,13 +1,13 @@
 const selectionList = ["rock", "paper", "scissors"];
-const playerSelection = "rock";
-const valueOfPlayerSelection = selectionList.find(element => {
-    return element.toLowerCase() === playerSelection.toLowerCase();
-});
 
 let playerScore = 0;
 let computerScore = 0;
 
 function playRound() {
+    let playerSelection = prompt("Choose rock, paper, or scissors");
+    const valueOfPlayerSelection = selectionList.find(element => {
+        return element.toLowerCase() === playerSelection.toLowerCase();
+    });
     const computerSelection = selectionList[Math.floor(Math.random() * selectionList.length)];
     if (valueOfPlayerSelection === "rock") {
         if (computerSelection === "paper") {
@@ -21,24 +21,22 @@ function playRound() {
         }
     } else if (valueOfPlayerSelection === "paper") {
         if (computerSelection === "scissors") {
-            score = 0;
+            computerScore++;
             return `Computer chose ${computerSelection}, you lose! Player Score: ${playerScore}, Computer Score: ${computerScore}`;
         } else if (computerSelection === "rock") {
-            score = 1;
+            playerScore++;
             return `Computer chose ${computerSelection}, you win! Player Score: ${playerScore}, Computer Score: ${computerScore}`;
         } else {
-            score = 0;
             return `Computer chose ${computerSelection}, it's a tie! Player Score: ${playerScore}, Computer Score: ${computerScore}`;
         }
     } else if (valueOfPlayerSelection === "scissors") {
         if (computerSelection === "rock") {
-            score = 0;
+            computerScore++;
             return `Computer chose ${computerSelection}, you lose! Player Score: ${playerScore}, Computer Score: ${computerScore}`;
         } else if (computerSelection === "paper") {
-            score = 1;
+            playerScore++;
             return `Computer chose ${computerSelection}, you win! Player Score: ${playerScore}, Computer Score: ${computerScore}`;
         } else {
-            score = 0;
             return `Computer chose ${computerSelection}, it's a tie! Player Score: ${playerScore}, Computer Score: ${computerScore}`;
         }
     } else {
